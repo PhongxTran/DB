f3502fc6f80d8714a1c6b842eb04446938973d88
Hoang Nguyen <nguyencanhhoangnh2019@gmail.com>
2021-11-26 19:02:01 +0700

    login

--- controllers/classroom/classroom.js
+++ controllers/classroom/classroom.js
@@ -2,6 +2,7 @@ let classroomModel = require('../../models/classroom/classroom')
 
 class classroomController {
     async getAllClassroom(req, res) {
+        // console.log(await classroomModel.getAllClassroom());
         res.render("classroom/classroom", {
             title: `DANH SÁCH PHÒNG HỌC`,
             classroomList: await classroomModel.getAllClassroom()
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,21 @@ var router = express.Router();
 /* GET home page. */
 // router.get('/', subjectController.getSubject);
 
+router.get('/login', function(req, res) {
+    res.render('login/index', { title: 'Login' });
+})
+
+router.get('/account/:mssv', function(req, res) {
+    let mssv = req.params.mssv;
+    res.render('account/index', { title: 'Account', mssv: mssv });
+})
+
+
+router.post('/login', function(req, res) {
+    let mssv = req.body.mssv;
+    let email = req.body.email;
+    res.redirect('/account/' + mssv);
+})
+
+
 module.exports = router;
